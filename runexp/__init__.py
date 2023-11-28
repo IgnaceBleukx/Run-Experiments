bfe418c7c6b51e129b09b4ebb20ffad05765458d
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2023-11-28 10:48:33 +0100

    fix num-workers argument

--- runexp/__init__.py
+++ runexp/__init__.py
@@ -13,5 +13,5 @@ def default_parser():
     parser.add_argument("output", type=str, help="Directory to output results of experiments")
     parser.add_argument("-u", "--unravel", action="store_true", help="Whether to unravel config file to run experiments in a batch (will unravel lists in configuration file to separate configs)")
     parser.add_argument("--parallel", action="store_true", help="Wheter to run experiments in paralell, only useful if `--unravel` is True")
-    parser.add_argument("--num-workers", action="store_const", const=cpu_count()-1, default=cpu_count()-1, help=f"Number of threads to use for parallelization (=nb of experiments running in parallel, default={cpu_count()-1}")
+    parser.add_argument("--num-workers", action="store", type=int, default=cpu_count()-1, help=f"Number of threads to use for parallelization (=nb of experiments running in parallel, default={cpu_count()-1}")
     return parser
